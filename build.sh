7ae0e1dce9e78eaec7a92b877b8f7979ca8fa1dd
Anton Johansson <97antjoh@gmail.com>
2021-07-01 22:53:57 +0200

    removed old build/reset files and changed configs

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git apply -3 --verbose patches/st-boxdraw_v2-0.8.3.diff
-git apply -3 --verbose patches/st-scrollback-20210507-4536f46.diff
-git apply -3 --verbose patches/st-scrollback-mouse-20210701-4536f46.diff
-git apply -3 --verbose patches/st-xresreload-20210701-4536f46.diff
-git apply -3 --verbose patches/st-newterm-0.8.2.diff
