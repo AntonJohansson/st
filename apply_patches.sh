dd498d37846ceee06087ee3d7b1752cf3b77b819
Anton Johansson <97antjoh@gmail.com>
2021-07-01 17:18:13 +0200

    changed aj config + removed vim patch which doesnt work

--- apply_patches.sh
+++ apply_patches.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-git apply --verbose patches/st-vim.diff
+cp config.aj.h config.h
+
+# Doesnt work for the current version
+#git apply --verbose patches/st-vim.diff
 git apply --verbose patches/newterm.diff
 git apply --verbose patches/st-xresreloading-4536f46.diff
+
+make -j4
+sudo make install
