6df7176f5a77a921fd9711ab16833b27acb219a1
Anton Johansson <97antjoh@gmail.com>
2021-07-01 17:29:02 +0200

    removed

--- apply_patches.sh
+++ apply_patches.sh
@@ -2,9 +2,10 @@
 
 cp config.aj.h config.h
 
-# Doesnt work for the current version
-#git apply --verbose patches/st-vim.diff
 git apply --verbose patches/newterm.diff
+git apply --verbose patches/st-boxdraw_v2-0.8.3.diff
+git apply --verbose patches/st-scrollback-20210507-4536f46.diff
+git apply --verbose patches/st-scrollback-mouse-20191024-a2c479c.diff
 git apply --verbose patches/st-xresreloading-4536f46.diff
 
 make -j4
