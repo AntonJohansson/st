a3baa4e76392af5f768e9277b5199439e8c138a5
Anton Johansson <97antjoh@gmail.com>
2021-07-01 18:30:51 +0200

    changed build order

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-cp config.aj.h config.h
-
-git apply --verbose patches/st-scrollback-20210507-4536f46.diff
-git apply --verbose patches/st-scrollback-mouse-20210701-4536f46.diff
-git apply --verbose patches/st-newterm-0.8.2.diff
-git apply --verbose patches/st-boxdraw_v2-0.8.3.diff
-git apply --verbose patches/st-xresreload-20210701-4536f46.diff
-
-make -j4
-sudo make install
+git apply -3 --verbose patches/st-boxdraw_v2-0.8.3.diff
+git apply -3 --verbose patches/st-scrollback-20210507-4536f46.diff
+git apply -3 --verbose patches/st-scrollback-mouse-20210701-4536f46.diff
+git apply -3 --verbose patches/st-xresreload-20210701-4536f46.diff
+git apply -3 --verbose patches/st-newterm-0.8.2.diff
