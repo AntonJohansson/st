3c412fe36a97419775f20707664d548058df3fd4
Anton Johansson <97antjoh@gmail.com>
2021-07-01 17:48:17 +0200

    changed patch order

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@
 
 cp config.aj.h config.h
 
-PATCHDIR=patches
-
-PATCHES=$(ls patches)
-for PATCH in $PATCHES; do
-	git apply --verbose $PATCHDIR/$PATCH
-done
+git apply --verbose patches/st-scrollback-20210507-4536f46.diff
+git apply --verbose patches/st-scrollback-mouse-20210701-4536f46.diff
+git apply --verbose patches/st-newterm-0.8.2.diff
+git apply --verbose patches/st-boxdraw_v2-0.8.3.diff
+git apply --verbose patches/st-xresreload-20210701-4536f46.diff
 
 make -j4
 sudo make install
